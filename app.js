6d8fd0623f49f9b1b449b3e89adfb9e7bdf01ecb
Antero Fagerstedt <antero.fagerstedt@iki.fi>
2017-10-09 22:53:20 +0300

    added maintenance page

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+const fs = require('fs');
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now} ${req.method} ${req.path}`
+    console.log(log);
+    fs.appendFile('/tmp/server.log', log + "\n", (err) => {
+        if (err) {
+            console.log('Could not write to log file');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('year', () => {
     return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamit', (text) => {
+    return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
